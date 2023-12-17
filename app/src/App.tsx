dcff5aceaccffba5d553dead6c06ed8b06d6ea57
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-17 21:08:08 +0200

    [routes]: instaled react-ruter-dom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 export default function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={'home'} />
+          <Route path="/feed" index element={'feed'} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
