3a49d07a3288a6c4c8d6fa30a96f77b9a19021d9
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-18 22:57:34 +0200

    [styles]: added styles to dialog

--- app/src/components/userProfile/index.tsx
+++ app/src/components/userProfile/index.tsx
@@ -4,7 +4,6 @@ import { User } from '@/types/user'
 import { useNavigate } from 'react-router-dom'
 import Button from '@/components/button'
 import { updateProfile } from '@/end-points'
-import { getLocal } from '@/utils'
 
 type Props = {
   user: User | null
