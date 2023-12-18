c99a18245d86b640cc880a5d6d3e1ac80736846c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-18 15:28:42 +0200

    [core]: allow user to regitser and login

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,33 +1,32 @@
-// const User = require('../models/user.model')
+import User from '../models/user.model.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 const login = async (req, res) => {
-  //   const { email, password } = req.body
-  //   // check if user is available in DB
-  //   const user = await User.findOne({ email })
-  //   if (!user) res.status(400).send({ message: 'Invalid email/password' })
-  //   // check if password is correct
-  //   const isValidPassword = await bcrypt.compare(password, user.password)
-  //   if (!isValidPassword) res.status(400).send({ message: 'Invalid email/password' })
-  //   const { password: hashedPassword, _id, ...userDetails } = user.toJSON()
-  //   // generate JWT token
-  //   const token = jwt.sign(
-  //     {
-  //       ...userDetails
-  //     },
-  //     process.env.JWT_SECRET,
-  //     { expiresIn: '2 days' }
-  //   )
-  //   res.status(200).send({
-  //     user: userDetails,
-  //     token
-  //   })
+  const { email, password } = req.body
+  // check if user is available in DB
+  const user = await User.findOne({ email })
+  if (!user) res.status(400).send({ message: 'Invalid email/password' })
+  // check if password is correct
+  const isValidPassword = await bcrypt.compare(password, user.password)
+  if (!isValidPassword) res.status(400).send({ message: 'Invalid email/password' })
+  const { password: hashedPassword, _id, ...userDetails } = user.toJSON()
+  // generate JWT token
+  const token = jwt.sign(
+    {
+      ...userDetails
+    },
+    'we_hit_those',
+    { expiresIn: '2 days' }
+  )
+  res.status(200).send({
+    user: userDetails,
+    token
+  })
 }
 
 const register = async (req, res) => {
-  //   const { email, password, firstName, lastName } = req.body
-  console.log(req.body)
+  const { email, password, firstName, lastName } = req.body
   if (!email || !password || !firstName || !lastName) {
     res.status(400).send({ message: 'all fields are required' })
   }
@@ -42,9 +41,7 @@ const register = async (req, res) => {
 
     await user.save()
 
-    console.log(email, password, firstName, lastName)
-
-    // res.status(200).send({ user })
+    res.status(200).send({ user })
   } catch (e) {
     res.status(500).send({ error: e })
   }
---
+++ server/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+})
+
+userSchema.pre(
+  'save',
+  async function (next) {
+    try {
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(this.password, salt)
+      next()
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
