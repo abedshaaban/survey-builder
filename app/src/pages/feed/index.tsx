207704054a217bfff6c6fda4c22d5a14c22dd3f4
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-20 13:27:20 +0200

    [styles]: added file styles and feed page

--- app/src/pages/feed/index.tsx
+++ app/src/pages/feed/index.tsx
@@ -19,18 +19,10 @@ export default function Feed() {
   }, [])
 
   return (
-    <div>
+    <div className="feed-page">
       {feed?.map((file: { title: string; _id: string }, i: number) => {
         return <File key={i} title={file.title} url={file._id} />
       })}
-
-      <Button
-        onClick={() => {
-          console.log(feed)
-        }}
-      >
-        log
-      </Button>
     </div>
   )
 }
