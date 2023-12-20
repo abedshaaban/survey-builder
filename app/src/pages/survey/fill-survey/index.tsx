bce6ff224f04eb3981480d83365bd43fc1843d9a
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-20 18:47:13 +0200

    [core]: render page

--- app/src/pages/survey/fill-survey/index.tsx
+++ app/src/pages/survey/fill-survey/index.tsx
@@ -1,3 +1,5 @@
+import './index.css'
+import { CheckBox, Radio, TextInput } from '@/components/form-content'
 import { GetSurveysById } from '@/end-points'
 import { Survey } from '@/types/survey'
 import { useEffect, useState } from 'react'
@@ -5,20 +7,34 @@ import { useParams } from 'react-router-dom'
 
 export default function Index() {
   const param = useParams()
-  const [survey, setSurvey] = useState<Survey | any>()
+  const [survey, setSurvey] = useState<Survey>()
   const id = param.id as string
 
   useEffect(() => {
     const getSurvey = async () => {
       const data = await GetSurveysById({ id: id })
 
-      setSurvey(data)
+      setSurvey(data?.data?.survey as Survey)
     }
 
     getSurvey()
   }, [])
 
-  console.log(survey)
+  return (
+    <div className="fill-survey-page">
+      <h1 className="fill-survey-page-h1">{survey?.title}</h1>
 
-  return <div>{id}</div>
+      {survey?.questions?.map((q, i) => {
+        if (q.type === 'text-input') {
+          return (
+            <TextInput key={i} title={q.title} placeholder={q.placeholder as string} />
+          )
+        } else if (q.type === 'radio') {
+          return <Radio key={i} title={q.title} choices={q.questions} />
+        } else if (q.type === 'check-box') {
+          return <CheckBox key={i} title={q.title} choices={q.questions} />
+        }
+      })}
+    </div>
+  )
 }
